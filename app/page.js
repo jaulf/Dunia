e66d071347f7a79185445c5b7dc4ebc47a9da6ce
howard otuya <howardotuya17@gmail.com>
2023-07-31 23:28:08 +0100

    Updated Landing Page: Added Footer

--- app/page.js
+++ app/page.js
@@ -19,6 +19,8 @@ import clover from "../public/images/clover.png";
 import d6 from "../public/images/D6.png";
 import d7 from "../public/images/D7.png";
 import ratings from "../public/images/ratings.png";
+import obfImg from "../public/images/obfImg.png";
+import memobf from "../public/images/mem-obf.png";
 
 import tp1 from "../public/images/tp1.png";
 import tp2 from "../public/images/tp2.png";
@@ -32,6 +34,7 @@ import c4 from "../public/images/c4.png";
 
 import hearte from "../public/images/heart-empty.png";
 import heartf from "../public/images/heart-full.png";
+import glasseffect from "../public/images/glasseffect.png";
 
 export default function Home() {
   // const router = useRouter()
@@ -46,7 +49,7 @@ export default function Home() {
           <nav className="flex justify-between py-[26px] px-10">
             <div className="flex gap-11 items-center">
               <Image
-                className="w-[91px] h-[28px]"
+                className="w-full max-w-[91px] h-auto"
                 src={dunia}
                 alt="Dunia Logo"
               />
@@ -141,24 +144,29 @@ export default function Home() {
                 <Image className="h-10 w-auto" src={hleftimg} alt="Memoji" />
               </div>
             </div>
-            <div id="h-ri" className="right rounded-[50px]">
+
+            <div id="h-ri" className="right relative rounded-[50px]">
               <div
                 id="card"
-                className="px-6 py-7 pb-[59px] flex flex-col gap-[30px]"
+                className="px-6 py-7 pb-[59px] relative z-50 flex flex-col gap-[30px]"
               >
                 <div className="flex gap-[10px]">
-                  <div>
+                  <div class="flex flex-col max-h-[552px]">
                     <Image
-                      className="rounded-[30px] w-auto h-auto max-h-[552px]"
+                      className="rounded-[30px] w-auto h-full max-h-[552px]"
+                      quality={100}
                       src={r1}
                       alt="one"
+                      placeholder="blur"
                     />
                   </div>
-                  <div>
+                  <div class="flex flex-col max-h-[552px]">
                     <Image
-                      className="rounded-[30px] w-auto h-auto max-h-[552px]"
+                      className="rounded-[30px] w-auto h-full max-h-[552px]"
+                      quality={100}
                       src={r2}
                       alt="two"
+                      placeholder="blur"
                     />
                   </div>
                 </div>
@@ -179,16 +187,29 @@ export default function Home() {
                   </div>
                 </div>
               </div>
+              <Image
+                className="object-cover rounded-[50px]"
+                fill
+                src={glasseffect}
+                alt="glasseffects"
+                sizes="100vw"
+                placeholder="blur"
+                quality={100}
+              />
             </div>
           </div>
           <Image
             src={curlyarrow}
             className="w-[121.339px] h-auto flex absolute bottom-[164px] left-[370px]"
             alt="Curly Arrow Icon"
+            placeholder="blur"
+            quality={100}
           />
           <Image
             src={clover}
             alt="Clover Icon"
+            placeholder="blur"
+            quality={100}
             className="w-10 h-10 flex self-start absolute top-[149px] left-[485px]"
           />
         </div>
@@ -247,7 +268,7 @@ export default function Home() {
           <div className="px-10 pt-[88px] pb-[134px] flex gap-12 flex-col">
             <div className="flex justify-between">
               <h3 className="millik text-[32px] text-black leading-[30.94px]">
-              Top picks this week🌟
+                Top picks this week🌟
               </h3>
               <Link
                 className="rounded-full font-bold py-[14px] px-6 leading-[19.36px] text-center text-[#009f00] border border-[#009f00]"
@@ -259,8 +280,18 @@ export default function Home() {
 
             <div className="grid gap-5 grid-cols-4">
               <div className="flex flex-col gap-4">
-                <div id="img-ccard" className="relative">
-                  <Image className="rounded-[30px]" src={tp1} alt="Top Pick One" />
+                <div
+                  id="img-ccard"
+                  className="relative inline-flex justify-center max-h-[400px] max-w-[325px] items-center flex-col"
+                >
+                  <Image
+                    className="rounded-[30px] w-full h-auto"
+                    sizes="100vw"
+                    placeholder="blur"
+                    src={tp1}
+                    quality={100}
+                    alt="Top Pick One"
+                  />
                   <div className="absolute top-6 right-6 rounded-full w-10 h-10 flex bg-white justify-center items-center">
                     <Image className="w-6 h-6" src={hearte} alt="Heart Icon" />
                   </div>
@@ -282,9 +313,9 @@ export default function Home() {
                       </span>
                     </div>
                     <p className="text-xs leading-5 text-[#121212]">
-                      As climate change grips the planet, here is Earth. Org&apos;s
-                      selection of must-read books on climate change and
-                      sustainability to enlighten you.
+                      As climate change grips the planet, here is Earth.
+                      Org&apos;s selection of must-read books on climate change
+                      and sustainability to enlighten you.
                     </p>
                     <div className="flex gap-[3px] items-center">
                       <Image
@@ -301,8 +332,18 @@ export default function Home() {
               </div>
 
               <div className="flex flex-col gap-4">
-                <div id="img-ccard" className="relative">
-                  <Image className="rounded-[30px]" src={tp2} alt="Top Pick Two" />
+                <div
+                  id="img-ccard"
+                  className="relative inline-flex justify-center max-h-[400px] max-w-[325px] items-center flex-col"
+                >
+                  <Image
+                    className="rounded-[30px] w-full h-auto"
+                    sizes="100vw"
+                    placeholder="blur"
+                    src={tp2}
+                    quality={100}
+                    alt="Top Pick Two"
+                  />
                   <div className="absolute top-6 right-6 rounded-full w-10 h-10 flex bg-white justify-center items-center">
                     <Image className="w-6 h-6" src={heartf} alt="Heart Icon" />
                   </div>
@@ -324,9 +365,9 @@ export default function Home() {
                       </span>
                     </div>
                     <p className="text-xs leading-5 text-[#121212]">
-                      As climate change grips the planet, here is Earth. Org&apos;s
-                      selection of must-read books on climate change and
-                      sustainability to enlighten you.
+                      As climate change grips the planet, here is Earth.
+                      Org&apos;s selection of must-read books on climate change
+                      and sustainability to enlighten you.
                     </p>
                     <div className="flex gap-[3px] items-center">
                       <Image
@@ -342,10 +383,16 @@ export default function Home() {
                 </div>
               </div>
               <div className="flex flex-col gap-4">
-                <div id="img-ccard" className="relative">
+                <div
+                  id="img-ccard"
+                  className="relative inline-flex justify-center max-h-[400px] max-w-[325px] items-center flex-col"
+                >
                   <Image
-                    className="rounded-[30px]"
+                    className="rounded-[30px] w-full h-auto"
+                    sizes="100vw"
+                    placeholder="blur"
                     src={tp3}
+                    quality={100}
                     alt="Top Pick Three"
                   />
                   <div className="absolute top-6 right-6 rounded-full w-10 h-10 flex bg-white justify-center items-center">
@@ -369,9 +416,9 @@ export default function Home() {
                       </span>
                     </div>
                     <p className="text-xs leading-5 text-[#121212]">
-                      As climate change grips the planet, here is Earth. Org&apos;s
-                      selection of must-read books on climate change and
-                      sustainability to enlighten you.
+                      As climate change grips the planet, here is Earth.
+                      Org&apos;s selection of must-read books on climate change
+                      and sustainability to enlighten you.
                     </p>
                     <div className="flex gap-[3px] items-center">
                       <Image
@@ -387,8 +434,18 @@ export default function Home() {
                 </div>
               </div>
               <div className="flex flex-col gap-4">
-                <div id="img-ccard" className="relative">
-                  <Image className="rounded-[30px]" src={tp4} alt="Top Pick Four" />
+                <div
+                  id="img-ccard"
+                  className="relative inline-flex justify-center max-h-[400px] max-w-[325px] items-center flex-col"
+                >
+                  <Image
+                    className="rounded-[30px] w-full h-full object-cover"
+                    sizes="100vw"
+                    placeholder="blur"
+                    src={tp4}
+                    quality={100}
+                    alt="Top Pick Four"
+                  />
                   <div className="absolute top-6 right-6 rounded-full w-10 h-10 flex bg-white justify-center items-center">
                     <Image className="w-6 h-6" src={hearte} alt="Heart Icon" />
                   </div>
@@ -410,9 +467,9 @@ export default function Home() {
                       </span>
                     </div>
                     <p className="text-xs leading-5 text-[#121212]">
-                      As climate change grips the planet, here is Earth. Org&apos;s
-                      selection of must-read books on climate change and
-                      sustainability to enlighten you.
+                      As climate change grips the planet, here is Earth.
+                      Org&apos;s selection of must-read books on climate change
+                      and sustainability to enlighten you.
                     </p>
                     <div className="flex gap-[3px] items-center">
                       <Image
@@ -449,18 +506,35 @@ export default function Home() {
                 See all
               </Link>
             </div>
-
             <div className="flex flex-col gap-[30px] pt-8">
               <div className="flex justify-between gap-8">
                 <div className="w-full h-full max-w-[534px] max-h-[629.32px] rounded-[48px] flex-col justify-end items-center inline-flex">
-                  <Image className="w-full rounded-[48px]" src={c1} alt="Category 1" />
+                  <Image
+                    className="w-full rounded-[48px]"
+                    src={c1}
+                    alt="Category 1"
+                    placeholder="blur"
+                    quality={100}
+                  />
                 </div>
                 <div className="flex flex-col justify-between">
                   <div className="w-full h-full max-w-[794px] max-h-[297px] rounded-[48px] flex-col justify-end items-center inline-flex">
-                    <Image className="w-full rounded-[48px]" src={c2} alt="Category 2" />
+                    <Image
+                      className="w-full rounded-[48px]"
+                      src={c2}
+                      alt="Category 2"
+                      placeholder="blur"
+                      quality={100}
+                    />
                   </div>
                   <div className="w-full h-full max-w-[794px] max-h-[297px] rounded-[48px] flex-col justify-end items-center inline-flex">
-                    <Image className="w-full rounded-[48px]" src={c3} alt="Category 3" />
+                    <Image
+                      className="w-full rounded-[48px]"
+                      src={c3}
+                      alt="Category 3"
+                      placeholder="blur"
+                      quality={100}
+                    />
                   </div>
                 </div>
               </div>
@@ -469,16 +543,137 @@ export default function Home() {
                   className="w-full rounded-[48px]"
                   src={c4}
                   alt="Category 4"
+                  placeholder="blur"
+                  quality={100}
                 />
               </div>
             </div>
+            <Image
+              className="absolute w-10 h-10 top-[14px] right-[529px]"
+              src={d7}
+              alt="Dark Green Clover"
+              placeholder="blur"
+              quality={100}
+            />
+          </div>
 
-            <Image className="absolute w-10 h-10 top-[14px] right-[529px]" src={d7} alt="Dark Green Clover" />
+          <div className="px-10 relative pt-[77px] pb-[140px] flex items-end gap-8">
+            <div class="flex w-full flex-col gap-11">
+              <div class="flex self-start flex-col gap-6">
+                <div className="flex flex-col gap-2">
+                  <h2 className="text-[56px] millik text-black leading-[64px]">
+                    Online Book Fair -{" "}
+                    <span class="block">The reader’s conference</span>
+                  </h2>
+                  <p className="text-[#4d4d4d] max-w-[612px]">
+                    As climate change grips the planet, here is Earth. Org's
+                    selection of must-read books on climate change and
+                    sustainability to enlighten you.
+                  </p>
+                </div>
+                <button className="text-xl flex self-start font-bold px-10 py-[14px] rounded-[32px] text-white bg-[#009F00] ">
+                  Join the community
+                </button>
+              </div>
+
+              <div class="bg-[#F2F2F2] w-full pb-8 pt-12 pl-[42px] rounded-[48px] flex flex-col gap-8">
+                <div class="flex flex-col gap-4">
+                  <h6 class=" text-xl font-medium text-black">Our community</h6>
+                  <Image
+                    class="h-10 w-auto self-start"
+                    placeholder="blur"
+                    sizes="100vw"
+                    quality={100}
+                    src={memobf}
+                    alt="members"
+                  />
+                </div>
+                <h3 class="text-[48px] text-black millik max-w-[619px]">
+                  Over 1.7K active readers & book lovers joined.
+                </h3>
+              </div>
+            </div>
+            <div className="w-full h-full max-w-[612px] max-h-[650px] relative rounded-[48px] overflow-hidden inline-flex justify-center items-center flex-col">
+              <Image
+                className="w-full object-cover h-auto rounded-[48px]"
+                alt="Online Book Fair"
+                src={obfImg}
+                sizes="100vw"
+                placeholder="blur"
+                quality={100}
+              />
+            </div>
+            <div class="absolute flex self-start top-[21px] left-[143px]">
+              <Image class="w-10 h-10" src={d7} alt="clover" />
+            </div>
+          </div>
+        </section>
+
+        <section className="bodyCon">
+          <div className="pt-[82px] pb-[132px] relative flex gap-6 justify-center items-center flex-col">
+            <div className="flex flex-col justify-center items-center gap-3 ">
+              <h3 className="millik text-[#121212] max-w-[845px] text-center text-[56px] leading-[64px]">
+                Be the first to know when new{" "}
+                <span class="dunia-grd2">resources</span> are available
+              </h3>
+              <p className="max-w-[612px] text-[#4d4d4d] text-center">
+                As climate change grips the planet, here is Earth. Org's
+                selection of must-read books on climate change and
+                sustainability to enlighten you.
+              </p>
+            </div>
+
+            <div className="flex flex-col gap-8">
+              <input
+                className="py-[14px] px-5 outline-none subscribe rounded-full"
+                type="email"
+                placeholder="Enter your email address"
+              />
+              <button className="bg-[#009F00] font-bold text-xl text-white py-[14px] px-12 self-center rounded-full flex justify-center items-center">
+                Subscribe
+              </button>
+            </div>
+
+            <div class="absolute flex self-start top-[38px] right-[158px]">
+              <Image class="w-10 h-10" src={d7} alt="clover" />
+            </div>
           </div>
         </section>
       </main>
 
-      <footer></footer>
+      <footer className="bg-[#009F00] overflow-hidden">
+        <div className="bodyCon ">
+          <div className="pt-[70px] px-14 relative pb-[658px] flex gap-[151px]">
+            <div>
+              <h1 className="foot-acc-left">
+                Climate literacy: Creative storytelling acceleration.
+              </h1>
+            </div>
+            <div class="foot-left flex gap-[72px]">
+              <div>
+                <h3>Dunia</h3>
+                <p>Terms of Service</p>
+                <p>Refund Policy</p>
+              </div>
+              <div>
+                <h3>Socials</h3>
+                <p>Instagram</p>
+                <p>Twitter</p>
+                <p>Youtube</p>
+                <p>LinkedIn</p>
+              </div>
+              <div>
+                <h3>Interact with us</h3>
+                <p>Shop</p>
+                <p>Contact us</p>
+              </div>
+            </div>
+            <div className="text-[560px] text-white leading-[542px] cent-dun self-center absolute millik font-normal tracking-[-5.6px]">
+              dunia
+            </div>
+          </div>
+        </div>
+      </footer>
     </div>
   );
 }
