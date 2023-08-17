797bfae98a444de7a8eeef3308be48af8b21fdf1
howard otuya <howardotuya17@gmail.com>
2023-08-17 15:20:22 +0100

    Onboarding flow Implementation - Signup

--- app/onboarding/signup/email/page.js
+++ app/onboarding/signup/email/page.js
@@ -2,6 +2,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import { useState } from "react";
+import { useRouter } from "next/navigation";
 import axios from "axios";
 axios.defaults.withCredentials = true;
 
@@ -13,24 +14,26 @@ import mail from "@/public/images/mail.png";
 import mail2 from "@/public/images/mail2.png";
 import arrowleft from "@/public/images/arrow-left.png";
 import passcode from "@/public/images/passcode.png";
-import back from "@/public/images/back.png"
-import user from "@/public/images/user.png"
+import back from "@/public/images/back.png";
+import user from "@/public/images/user.png";
+import errorCircle from "@/public/images/error-circle.png";
+import successCircle from "@/public/images/success-circle.png";
+import close from "@/public/images/x.png";
 
 export default function Home() {
+  const router = useRouter();
+
   // useState
   const [fname, setFname] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [errorM, setErrorM] = useState("");
+  const [successM, setSuccessM] = useState("");
 
   // URL
   const url = process.env.NEXT_PUBLIC_BASE_URL + "/api/v1/register";
 
   // Header Definition
-  const headers = {
-    "Content-Type": "application/json",
-    Accept: "application/json",
-  };
-
   const config = {
     headers: {
       "Content-Type": "application/json",
@@ -49,20 +52,98 @@ export default function Home() {
   const submitHandler = async (e) => {
     e.preventDefault();
 
-    // const result = await fetch(url, {
-    //   method: "POST",
-    //   headers,
-    //   body: JSON.stringify(body),
-    // });
-
     axios
       .post(url, body, config)
-      .then((response) => console.log(response.data))
-      .catch((e) => console.log(e.response.data.errors));
+      .then((response) => {
+        localStorage.setItem("User", response.data)
+        setErrorM("");
+        setSuccessM("Account created. You will be redirected shortly.");
+        setTimeout(() => {
+          router.push("/profile");
+        }, 3000);
+      })
+      .catch((e) => {
+        setSuccessM("");
+        setErrorM(e.response.data.message);
+      });
+  };
+
+  const closeErrorModal = () => {
+    const popup = document.getElementById("error");
+    if (popup.style.display == "none") {
+      popup.style.display = "flex";
+    } else {
+      popup.style.display = "none";
+    }
+  };
+
+  const closeSuccessModal = () => {
+    const popup = document.getElementById("success");
+    if (popup.style.display == "none") {
+      popup.style.display = "flex";
+    } else {
+      popup.style.display = "none";
+    }
   };
 
   return (
     <>
+      <div
+        id="State-Manager"
+        className="fixed z-[99999] top-16 right-14 flex flex-col gap-4"
+      >
+        {errorM && (
+          <div
+            id="error"
+            className="border-l-4 bg-[#FDEFED] border-[#EA5945] p-4 shadow-md flex flex-row gap-14 items-center rounded-[4px] overflow-hidden"
+          >
+            <div className="flex flex-row gap-4 items-center">
+              <div className="w-8 h-8 grid grid-cols-1">
+                <Image
+                  className="w-full h-auto"
+                  src={errorCircle}
+                  alt="Error"
+                />
+              </div>
+              <div className="flex flex-col gap-2">
+                <h3 className="font-semibold text-[#121212]">Error</h3>
+                <p className=" text-sm leading-[119%] text-[#4d4d4d]">
+                  {errorM}
+                </p>
+              </div>
+            </div>
+            <div onClick={closeErrorModal} className="grid grid-cols-1">
+              <Image className="w-5 h-5" src={close} alt="close" />
+            </div>
+          </div>
+        )}
+        {successM && (
+          <div
+            id="success"
+            className="border-l-4 bg-[#EDFDF9] border-[#13B288] p-4 shadow-md flex flex-row gap-14 items-center rounded-[4px] overflow-hidden"
+          >
+            <div className="flex flex-row gap-4 items-center">
+              <div className="w-8 h-8 grid grid-cols-1">
+                <Image
+                  className="w-full h-auto"
+                  src={successCircle}
+                  alt="Success"
+                />
+              </div>
+              <div className="flex flex-col gap-2">
+                <h3 className="font-semibold text-[#121212]">Success</h3>
+                <p className=" text-sm leading-[119%] text-[#4d4d4d]">
+                  {successM}
+                </p>
+              </div>
+            </div>
+            <div onClick={closeSuccessModal} className="grid grid-cols-1">
+              <Image className="w-5 h-5" src={close} alt="close" />
+            </div>
+          </div>
+        )}
+      </div>
+
       <header className="header-grdi">
         <div
           id="Image-sidebar"
@@ -97,7 +178,9 @@ export default function Home() {
           </Link>
 
           <div className="px-[140px] pt-8 flex flex-col justify-center items-center">
-            <h2 className="millik text-[64px] leading-[62px] tracking-[-0.64px]">dunia</h2>
+            <h2 className="millik text-[64px] leading-[62px] tracking-[-0.64px]">
+              dunia
+            </h2>
 
             <div className="flex flex-col justify-center items-center gap-8 pt-16">
               <h2 className="text-[28px] font-medium leading-8 text-center">
@@ -108,22 +191,22 @@ export default function Home() {
                 onSubmit={submitHandler}
                 className="w-[389px] flex justify-center items-center flex-col gap-5"
               >
-              <div className="flex whitespace-nowrap rounded-full gap-[10px] items-center w-full max-w-[389px] px-[35px] py-5 border-[1.5px] border-[#d9d9d9]">
-                <Image
-                  quality={100}
-                  className="w-6 h-6"
-                  src={user}
-                  alt="User Icon"
-                />
-                <input
-                  id="fname"
-                  className="outline-none w-full"
-                  type="text"
-                  placeholder="Full Name"
-                  onChange={(e) => setFname(e.target.value)}
-                  required
-                />
-              </div>
+                <div className="flex whitespace-nowrap rounded-full gap-[10px] items-center w-full max-w-[389px] px-[35px] py-5 border-[1.5px] border-[#d9d9d9]">
+                  <Image
+                    quality={100}
+                    className="w-6 h-6"
+                    src={user}
+                    alt="User Icon"
+                  />
+                  <input
+                    id="fname"
+                    className="outline-none w-full"
+                    type="text"
+                    placeholder="Full Name"
+                    onChange={(e) => setFname(e.target.value)}
+                    required
+                  />
+                </div>
                 <div className="flex whitespace-nowrap rounded-full gap-[10px] items-center w-full max-w-[389px] px-[35px] py-5 border-[1.5px] border-[#d9d9d9]">
                   <Image
                     quality={100}
@@ -164,7 +247,7 @@ export default function Home() {
               </form>
 
               <div className="font-medium text-[#666] flex flex-col justify-center items-center gap-10">
-              <div className="text-sm leading-6 w-full lg:max-w-[300px] text-center">
+                <div className="text-sm leading-6 w-full lg:max-w-[300px] text-center">
                   By continuing, you agree to our company’s{" "}
                   <span className="text-[#F2665B] font-medium">
                     Terms of Use, Conditions
--- app/onboarding/signup/page.js
+++ app/onboarding/signup/page.js
@@ -10,6 +10,8 @@ import Link from "next/link";
 export default function Home() {
   return (
     <>
+
+    
       <header className="header-grdi">
         <div
           id="Image-sidebar"
